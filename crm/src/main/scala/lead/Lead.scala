8bc36b94d37f1ceaf2bfe83268f9e2633a06fa87
roshansharma <rosharma9@gmail.com>
2015-06-19 16:28:16 +0545

    Added Events

--- crm/src/main/scala/lead/Lead.scala
+++ crm/src/main/scala/lead/Lead.scala
@@ -5,6 +5,10 @@ import domain.BaseAggregateRoot
 /**
  * Created by roshansharma on 6/18/15.
  */
-case class Lead(id: String) extends BaseAggregateRoot {
+case class Lead(id: String,
+                name: String,
+                isConverted: Boolean = false,
+                convertedTo: Option[String] = None) extends BaseAggregateRoot {
 
+  def convert(to: String) = this.copy(isConverted = true, convertedTo = Some(to))
 }
